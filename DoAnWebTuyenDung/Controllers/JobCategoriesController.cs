fa4033506ce32504e8b1fbbf3ec2ed945ea18c5e
Trang07102005 <ltttrang0710@gmail.com>
2024-10-28 15:13:11 +0700

    sua1

---
+++ DoAnWebTuyenDung/Controllers/JobCategoriesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoAnWebTuyenDung.Controllers
+{
+    public class JobCategoriesController : Controller
+    {
+        // GET: JobCategories
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
